4628b9caed6df76416b611b80536d990eefe8838
Ivan Prilepsky <prilepskii.ivan@yandex.ru>
2023-08-29 13:35:37 +0300

    add new ref

--- Capture screen&avg intencity.py
+++ Capture screen&avg intencity.py
@@ -4,36 +4,21 @@ from mss import mss
 # from PIL import Image
 
 bounding_box = {'top': 400, 'left': 800, 'width': 400, 'height': 300}
-
 sct = mss()
-
-# Initialize the list of average intensity values
-average_intensities = []
+average_intensities = [] # Initialize the list of average intensity values
 
 while True:
     sct_img = sct.grab(bounding_box)
-
-    # Convert the image to a numpy array
-    image_array = np.array(sct_img)
-
-    # Calculate the average intensity value for the image
-    average_intensity = np.mean(image_array)
-
-    # Add the average intensity value to the list
-    average_intensities.append(average_intensity)
-
+    image_array = np.array(sct_img) # Convert the image to a numpy array
+    average_intensity = np.mean(image_array) # Calculate the average intensity value for the image
+    average_intensities.append(average_intensity) # Add the average intensity value to the list
     cv2.imshow('screen', np.array(sct_img))
 
     if (cv2.waitKey(1) & 0xFF) == ord('q'):
         cv2.destroyAllWindows()
-
-        # Output the average intensity
-        # Define the folder path and file name
-        folder_path = "/Users/User/Desktop/Lab17/Git_Projects"
-        file_name = "signal.dat"
-
-        # Save the array to a .dat file
+        folder_path = "/Users/User/Desktop/Lab17/Git_Projects" # Output the average intensity
+        file_name = "signal.dat" # Define the folder path and file name
         file_path = folder_path + "/" + file_name
-        np.savetxt(file_path, average_intensities)
+        np.savetxt(file_path, average_intensities) # Save the array to a .dat file
 
         break
